207e1afd22fb30bd3d3b383e8467f28f9345e3df
5DavisSt <splinter.hopkins@gmail.com>
2024-04-08 18:35:41 +0200

    Added the React Navigation component, and some CSS for the first custom component

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Home from './src/Page/Home/Home.jsx';
 import { StatusBar } from 'expo-status-bar';
-//import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Home />
-);
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 //pas besoin ?
--- src/Component/Home/BannerSection.js
+++ src/Component/Home/BannerSection.js
@@ -10,15 +10,11 @@ const BannerSection = () => {
   return (
     <View style={styles.bannerVideo}>
 
-      <View className="container container-two">
-        <View className="row justify-content-center">
-          <View className="col-lg-8">
-            <View className="banner-video__content text-center">
-              <Text className="banner-video__subtitle bg-gray-800 text-uppercase font-14  text-white">FinTech Fusion</Text>
-              <Text className="banner-video__title text-white">Land Organization find your fantasy house with us</Text>
-              <Text className="banner-video__desc font-18 text-white">Unlock the Power of Real Estate Making Your Real Estate Dreams a Reality Real Estate here Unlock the Power of Real Estate</Text>
-            </View>
-          </View>
+      <View style={styles.container}>
+        <View style={styles.bannerVideoContent}>
+          <Text style={styles.bannerVideoSubtitle}>FinTech Fusion</Text>
+          <Text style={styles.bannerVideoTitle}>Land Organization find your fantasy house with us</Text>
+          <Text style={styles.bannerVideoDesc}>Unlock the Power of Real Estate Making Your Real Estate Dreams a Reality Real Estate here Unlock the Power of Real Estate</Text>
         </View>
       </View>
     </View>
@@ -27,8 +23,52 @@ const BannerSection = () => {
 
 const styles = StyleSheet.create({
   bannerVideo: {
-    padding: 140,
+    backgroundColor: 'lightgray',
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingRight: 'calc(1.5 * 0.5)',
+    paddingLeft: 'calc(1.5 * 0.5)',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+ },
+  container: {
+    width: '100%',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
   },
+  bannerVideoContent: {
+    textAlign: 'center',
+    alignContent: 'center'
+  },
+  bannerVideoSubtitle: {
+    textAlign: 'center',
+    color: 'white',
+    padding: 10,
+    borderRadius: 3,
+    marginBottom: 10,
+    letterSpacing: 0.11,
+    fontSize: 20,
+    textTransform: 'uppercase',
+  },
+  bannerVideoTitle: {
+    textAlign: 'center',
+    marginTop: 8,
+    marginBottom: 32,
+    color: 'white',
+    fontSize: 22,
+    fontWeight: '400',
+  },
+  bannerVideoDesc: {
+    alignItems: 'center',
+    textAlign: 'center',
+    color: 'white',
+    maxWidth: '600px',
+    margin: '0 auto',
+    fontSize: 18,
+    fontWeight: '300',
+  },  
 });
 
 export default BannerSection;
