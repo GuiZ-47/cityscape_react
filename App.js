3312063b14d3b6fab9b2deb2b15b6338324a27d8
ASSANEASETO <157792659+ASSANEASETO@users.noreply.github.com>
2024-04-10 18:42:40 +0200

    first commit
    faire la page property

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Property from './src/Pages/Property';
+
 
 export default function App() {
   return (
 
-//    <View style={styles.container}>
-//      <Text>Open up App.js to start working on your app!</Text>
-//      <StatusBar style="auto" />
-//    </View>
-//
+  //<View style={styles.container}>
+  //<Text>Open up App.js to start working on your app!</Text>
+  //<StatusBar style="auto" />
+  //</View>
+
+  <Property />
+  //<PropertyList />
+
 );
 }
 
